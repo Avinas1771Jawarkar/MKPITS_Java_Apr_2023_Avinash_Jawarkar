1fc1c57f74a3937e73303d0442e3e0e105a00738
avinash <avinash01jawarkar@gmail.com>
2023-06-16 11:29:53 +0530

    example of inheritance

---
+++ JAVA/paperinheritance.java
@@ -0,0 +1,26 @@
+class paper{
+string colour;
+int length;
+int width;
+int thickness;
+}
+class newspaper{
+string nameofpaper;
+int length of paper;
+int width of new paper;
+}
+class hindu extend newspaper{
+string language;
+int size of word;
+int no. of limited pages;
+} 
+class exampaper{
+string subject;
+int no. of pages;
+int sizeofpaper;
+}
+class xeroxpaper{
+string thickness;
+string clour ;
+int size of paper;
+}
---
+++ JAVA/socialmediaInheritance.java
@@ -0,0 +1,21 @@
+classsocial media{
+string post;
+string video;
+string chatting;
+aratical;
+}
+class ofically{
+string chattin;
+ string news;
+ string paper;
+}
+class newpaper extend ofically{
+string language;
+int no. of limited pages;
+int size of word;
+}
+class enterinment{
+string type of video;
+string type of post;
+string time of post;
+}
