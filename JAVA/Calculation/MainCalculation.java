e448fc5c58578f89a64d732ba12b3b0be7bc4fa2
avinash <avinash01jawarkar@gmail.com>
2023-08-08 14:31:26 +0530

    Abstraction

--- JAVA/Calculation/MainCalculation.java
+++ JAVA/Calculation/MainCalculation.java
@@ -8,54 +8,64 @@ import java.util.Scanner;
 public class MainCalculation {
 
     public static void main(String[] args){
-        int result;
+//        int result;
+//
+//        Calculation s;
+//
+//
+//        Scanner scanner=new Scanner(System.in);
+//        System.out.println("Enter 1-Addition, 2-Substraction, 3-Multiplaction, 4-Division ");
+//        int choice = scanner.nextInt();
+//        switch (choice)
+//        {
+//            case 1:
+//                s=new Addition();
+//                System.out.println("enter the 2 number");
+//               result =s.calculate (scanner.nextInt(), scanner.nextInt());
+//                System.out.println(result);
+//                break;
+//
+//                case 2:
+//                    s=new Substraction();
+//                    System.out.println("enter the 2 number");
+//                     result = s.calculate(scanner.nextInt(), scanner.nextInt());
+//                    System.out.println(result);
+//                    break;
+//
+//                    case 3:
+//            s=new Multiplication();
+//            System.out.println("enter the 2 number");
+//            result = s.calculate(scanner.nextInt(), scanner.nextInt());
+//            System.out.println(result);
+//            break;
+//
+//            case 4:
+//                s=new Division();
+//                System.out.println("enter the 2 number");
+//                 result = s.calculate(scanner.nextInt(), scanner.nextInt());
+//                System.out.println(result);
+//                break;
+//
+//            default:
+//                System.out.println("invalid ");
+//
+//
+//        }
+//
+//
 
-        Calculation s;
-
-
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter 1-Addition, 2-Substraction, 3-Multiplaction, 4-Division ");
-        int choice = scanner.nextInt();
-        switch (choice)
-        {
-            case 1:
-                s=new Addition();
-                System.out.println("enter the 2 number");
-               result =s.calculate (scanner.nextInt(), scanner.nextInt());
-                System.out.println(result);
-                break;
-
-                case 2:
-                    s=new Substraction();
-                    System.out.println("enter the 2 number");
-                     result = s.calculate(scanner.nextInt(), scanner.nextInt());
-                    System.out.println(result);
-                    break;
-
-                    case 3:
-            s=new Multiplication();
-            System.out.println("enter the 2 number");
-            result = s.calculate(scanner.nextInt(), scanner.nextInt());
-            System.out.println(result);
-            break;
-
-            case 4:
-                s=new Division();
-                System.out.println("enter the 2 number");
-                 result = s.calculate(scanner.nextInt(), scanner.nextInt());
-                System.out.println(result);
-                break;
-
-            default:
-                System.out.println("invalid ");
-
-
-        }
 
 
 
 
     }
+
+
+
+
+
+
+
 
 }
 
