07a596b44c19696eb2f46ae99dc69f596b7761fb
avinash <avinash01jawarkar@gmail.com>
2023-08-26 17:55:15 +0530

    Math Classes

---
+++ JAVA/Classes/Math_Method.java
@@ -0,0 +1,115 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Math_Method {
+    public static void main(String[] args){
+
+
+        int NUMBER=25;
+        System.out.println(Math.sqrt(NUMBER));          //MATH FUNCTION
+
+         double NUMBER1= 10928;
+         double NUMBER2=838598;
+        System.out.println(Math.max(NUMBER1,NUMBER2)); // MAXIMUN (MATH FUNCTION// )
+        System.out.println(Math.min(NUMBER1,NUMBER2)); //MINUM (MATH FUNCTION)
+
+        long NUMBER3= 3;
+        System.out.println(Math.tan(NUMBER3));
+        System.out.println(Math.floor(NUMBER3));       // floor (MATH FUNCTION)
+        System.out.println(Math.exp(NUMBER3));         //EXPONITIAL(MATH FUNCTION)
+
+        System.out.println(Math.ceil(NUMBER3));
+        System.out.println(Math.cosh(NUMBER3));
+        System.out.println(Math.random());
+        System.out.println(Math.ulp(NUMBER3));
+        System.out.println(Math.toIntExact(NUMBER3));
+
+        double NUMBER4=12;
+        System.out.println(Math.toRadians(NUMBER4));
+int NUMBER5= 2;
+        System.out.println(Math.pow(NUMBER3, NUMBER2));
+
+
+
+
+
+
+
+
+
+
+        String str=new String( );
+        System.out.println(str);
+
+String str2= "Avinash";
+        String str1 =new String(str2);
+        System.out.println(str2);
+
+
+        char ch[]={'a','b','c','d'};
+        String srt3=new String(ch);
+        System.out.println(srt3);
+
+
+        String str5 = new String("Jawarkar");
+        String str6=new String("Avinash");
+
+        System.out.println(str5.concat( " " +str6) );
+
+
+
+        String str4=new String(str2);
+        System.out.println(str4.equals(str2));
+
+String str7=new String( str5);
+String str8=new String(str6);
+        System.out.println(str5.compareTo(str6));
+        System.out.println(str6.compareToIgnoreCase(str7));
+        System.out.println(str4.contains(str7));
+        System.out.println(str4.contentEquals(str7));
+   System.out.println(str4.contentEquals(str2));
+
+
+
+
+
+
+       char avi[]={ 'a','s','d','f','g'};
+       String str10=new String(avi);
+        System.out.println(str10.charAt(3));
+
+        System.out.println(str7.hashCode());
+        System.out.println(str4.isEmpty());
+        System.out.println(str4.lastIndexOf(str2));
+        System.out.println(str4.lastIndexOf(str4, 3));
+        System.out.println(str4.length());
+        System.out.println(str5.lines());
+        System.out.println(str5.matches(str4));
+//        System.out.println(str5.regionMatches());
+
+//       public  class Interger {
+//            int STR = 12;
+//            Interger B=new Interger(STR);
+//            System.out.println(B);
+
+//
+//        }
+
+
+//        int NUM=2;
+// Avi=new String();
+//        System.out.println(String.);
+
+
+//String STR ="i am avinash";
+//        System.out.println(STR.contains("avinash"));
+//
+//        char[]={ }
+
+
+
+
+
+
+
+    }
+}
