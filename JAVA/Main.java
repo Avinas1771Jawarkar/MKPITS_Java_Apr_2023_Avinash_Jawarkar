38664f0d0021060b8358c57709bfc6ae604d6c7c
avinash <avinash01jawarkar@gmail.com>
2023-09-01 16:27:08 +0530

    Array

---
+++ JAVA/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] arg){
+
+        int arr[][] =new int [5][5];
+
+        for(int row=0 ; row<4 ;row++){
+
+            for ( int coloum=0 ; coloum<5 ;coloum++){
+
+
+                System.out.println("Array["+ row +"] ["+ coloum +"] ,value :" +[row][coloum]);
+            }
+
+        }
+    }
+}
