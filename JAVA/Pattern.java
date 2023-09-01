edcad165ab10199fc989761afc406efb2244a319
avinash <avinash01jawarkar@gmail.com>
2023-09-01 17:05:16 +0530

    Pattern Program

---
+++ JAVA/Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern {
+
+    public static void main(String[] args){
+        for(row =0; row<5;row++){
+
+            for (coloum=0; coloum<5; coloum++) {
+                if (coloum = middle - row) ;
+            System.out.println(" * ")
+
+
+            }
+        }
+    }
+}
