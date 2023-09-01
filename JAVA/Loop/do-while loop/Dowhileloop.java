d79815cd5e96706cf8ffd3aef73952813cbcae33
avinash <avinash01jawarkar@gmail.com>
2023-09-01 17:10:49 +0530

    DoWhile

---
+++ JAVA/Loop/do-while loop/Dowhileloop.java
@@ -0,0 +1,17 @@
+class Dowhileloop{ 
+public static void main(String []args){
+int counter =1;
+
+do{
+	System.out.println("snooz");
+	counter++;
+	if(counter>5);
+System.out.println("the alarm is stop");
+}
+while(counter>5);
+
+	
+//System.out.println("alarm");
+	
+}
+}
