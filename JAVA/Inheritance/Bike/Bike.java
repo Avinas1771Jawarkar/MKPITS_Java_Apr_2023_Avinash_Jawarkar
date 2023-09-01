13f45e883c3de63c94f31c6798a327495365a929
avinash <avinash01jawarkar@gmail.com>
2023-09-01 15:38:00 +0530

    Single Inheritance

---
+++ JAVA/Inheritance/Bike/Bike.java
@@ -0,0 +1,16 @@
+package Bike;
+
+
+public class Bike{
+    void run(){System.out.println("running");}
+}
+
+class Honda extends Bike{
+    void run(){
+        System.out.println("running safely with 100kmph");
+    }
+
+}
+
+
+
---
+++ JAVA/Inheritance/Bike/BikeMain.java
@@ -0,0 +1,9 @@
+package Bike;
+
+public class BikeMain {
+    public static void main(String args[]) {
+        Honda honda = new Honda();
+        honda.run();
+    }
+}
+
