896a557f49b7a15e3769bfd048e8829efe44ba76
avinash <avinash01jawarkar@gmail.com>
2023-09-26 22:12:57 +0530

    unicode Escape Sequence

---
+++ JAVA/UncodeEscapeSequence/UnicodeEscapeSquence.java
@@ -0,0 +1,9 @@
+package UncodeEscapeSequence;
+
+public class UnicodeEscapeSquence {
+    public static void main(String[] args){
+
+        char ch='\u0009';
+        System.out.println(ch);
+    }
+}
