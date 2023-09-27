f15da7b4b01d95d01989bd399f72a77e9c502040
avinash <avinash01jawarkar@gmail.com>
2023-09-27 22:21:01 +0530

    Assignment

---
+++ JAVA/Assignment/Factorial.java
@@ -0,0 +1,19 @@
+public class Factorial {
+
+        static int factorial(int n){
+            if (n == 0)
+                return 1;
+            else
+                return(n * factorial(n-1));
+        }
+        public static void main(String args[]){
+            int i,fact=1 , fact1=1;
+            int number=5;//It is the number to calculate factorial
+            fact = factorial(number);
+            fact1= factorial(number);
+            System.out.println("Factorial of "+number+" is: "+fact);
+            System.out.println("Factorial of "+number+" is: "+fact1);
+        }
+    }
+
+
